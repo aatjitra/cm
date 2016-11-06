e5e8596a1284c338f3e6314ed2fa140cd800b978
aatjitra <aatjitra@gmail.com>
2016-11-06 16:46:43 +0000

    fix cherry-pick error

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -628,18 +628,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
-<<<<<<< HEAD
-=======
-	/* Ensure low persistence is disabled */
-	mdss_dsi_panel_apply_display_setting(pdata, 0);
-
-	display_on = true;
-
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
 
->>>>>>> 0411021... kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.5 (faux123/Yank555.lu)
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -692,15 +684,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 	if (ctrl->off_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
 
-<<<<<<< HEAD
-=======
-	display_on = false;
-
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
 #endif
 
->>>>>>> 0411021... kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.5 (faux123/Yank555.lu)
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_BLANK;
 	pr_debug("%s:-\n", __func__);
